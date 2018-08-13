06a58b7f5d058d4a53c42e8c9d3869234632f4ac
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-08-12 23:28:33 -0700

    added user interface for user

--- MemeMeV1/MemeMeV1/ViewController.swift
+++ MemeMeV1/MemeMeV1/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblTop: UILabel!
+    @IBOutlet weak var lblBottom: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
